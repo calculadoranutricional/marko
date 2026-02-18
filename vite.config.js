02f04d58acd6a884837c0071c197145dc4248368
calculadoranutricional <calculadoranutricional@gmail.com>
2026-02-18 14:44:38 -0300

    Auto-update: 14:44:38

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,16 @@ import marko from "@marko/run/vite";
 import adapter from "@marko/run-adapter-static";
 
 export default defineConfig({
-  base: '/marko/',
+  base: process.env.NODE_ENV === 'production' ? '/marko/' : '/',
   plugins: [
     marko({
       adapter: adapter()
     })
-  ]
+  ],
+  server: {
+    watch: {
+      // Evitamos que Vite local vigile la carpeta dist
+      ignored: ['**/dist/**']
+    }
+  }
 });
