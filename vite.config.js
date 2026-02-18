da4ebae87bdb52a592f6bf73c86d21cf95c0fa55
calculadoranutricional <calculadoranutricional@gmail.com>
2026-02-18 14:14:26 -0300

    fix: add base path for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import marko from "@marko/run/vite";
 import adapter from "@marko/run-adapter-static";
 
 export default defineConfig({
-  // 'base' debe ser el nombre de tu repo para que los archivos carguen bien en GitHub Pages
-  base: '/marko/', 
+  base: '/marko/',
   plugins: [
     marko({
       adapter: adapter()
