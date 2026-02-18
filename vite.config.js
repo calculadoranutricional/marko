5336cc6904e51f0fe501002515d4bf3f387c8808
calculadoranutricional <calculadoranutricional@gmail.com>
2026-02-18 14:51:54 -0300

    Auto-update: 14:51:54

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,18 @@ import { defineConfig } from "vite";
 import marko from "@marko/run/vite";
 import adapter from "@marko/run-adapter-static";
 
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/marko/' : '/',
+export default defineConfig(({ command }) => ({
+  // Si estamos construyendo para GitHub usa /marko/, si es dev usa /
+  base: command === 'build' ? '/marko/' : '/',
   plugins: [
     marko({
       adapter: adapter()
     })
   ],
   server: {
+    // Forzamos a Vite a ignorar la carpeta dist para que no se rompa el local
     watch: {
-      // Evitamos que Vite local vigile la carpeta dist
       ignored: ['**/dist/**']
     }
   }
-});
+}));
